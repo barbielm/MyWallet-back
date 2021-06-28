33b3377d5c54e7bfbc7427ef41898420738922b1
barbielm <barbosagabriel0508@gmail.com>
2021-06-27 22:45:41 -0300

    adicionar entrada e saída

--- src/server.js
+++ src/server.js
@@ -51,6 +51,51 @@ app.post('/sign-up', async (req,res) => {
     }
 })
 
+app.get('/extracts', async (req,res) => {
+    try{
+        const token = req.headers.authorization.substring(7,);
+        const extracts = await connection.query(`SELECT extracts.* FROM users JOIN sessions ON users.id = sessions."userId" JOIN extracts ON extracts."userId" = users.id WHERE sessions.token = $1`,[token]);
+        res.send(extracts.rows);
+    } catch {
+        res.sendStatus(500);
+    }
+})
+
+app.post('/add-deposit', async (req,res) =>{
+    try{
+        const token = req.headers.authorization.substring(7,);
+        const user = await connection.query(`SELECT * FROM sessions WHERE token = $1`,[token]);
+        if(!!user.rows[0]){
+            const {value, description, date, isDeposit} = req.body;
+            const addDeposit = await connection.query(`INSERT INTO extracts(value, description, date, "isDeposit", "userId") VALUES ($1,$2,$3,$4,$5)`,[value, description, date, isDeposit, user.rows[0].userId]);
+            res.sendStatus(201);
+        } else {
+            res.sendStatus(500);
+        }
+    } catch {
+        res.sendStatus(500);
+    }
+})
+
+app.post('/add-withdraw', async (req,res) =>{
+    try{
+        const token = req.headers.authorization.substring(7,);
+        console.log(token)
+        const user = await connection.query(`SELECT * FROM sessions WHERE token = $1`,[token]);
+        console.log(user.rows)
+        if(!!user.rows[0]){
+            const {value, description, date, isDeposit} = req.body;
+            const addDeposit = await connection.query(`INSERT INTO extracts(value, description, date, "isDeposit", "userId") VALUES ($1,$2,$3,$4,$5)`,[value, description, date, isDeposit, user.rows[0].userId]);
+            res.sendStatus(201);
+        } else {
+            res.sendStatus(500);
+        }
+    } catch(e) {
+        console.log(e);
+        res.sendStatus(500);
+    }
+})
+
 app.post('/logout', async (req,res) => {
     const token = req.headers.authorization.substring(7,); 
     try{
